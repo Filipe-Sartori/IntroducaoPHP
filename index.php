6330ca5b56b8b462390b0e89eca23d20ebc31def
Filipe-Sartori <filipeasvsabino@gmail.com>
2022-05-07 16:18:28 -0300

    Criação de serviços e funções php

--- index.php
+++ index.php
@@ -1,20 +1,40 @@
+<?php
+include "servicos/servicoMensagemSessao.php";
+?>
 <!DOCTYPE html>
-<html lang="en">
-<head>
+<html>
 
-    <meta charset="UTF-8">
-    <title>Formulário de Inscrição</title>
-    <meta name="autor" content="">
-    <meta name="viewport" content="">
-    <meta nome="description" content="width-device.width, initial-scale-1">
-    
+<head>
+    <meta charset="utf-8">
+    <title>Formulário de inscrição</title>
+    <meta name="author" content="">
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 </head>
+
 <body>
-    <p>Formulário para inscrição de competidores</p>
-    <form action="script.php" method="POST"><!-- action é responsavel de processar dados que seram enviado pelo metodo POST -->
-        <p>Seu Nome: <input type="text" name="nome" /></p>
-        <p>Sua idade: <input type="text" name="idade" /></p>
-        <p><input type="submit" value="Enviar dados co competidor"/></p>
-    </form>
+
+<p>FORMULÁRIO PARA INSCRIÇÃO DE COMPETIDORES</p>
+
+<form action="script.php" method="post">
+    <?php
+        $mensagemDeSucesso = obterMensagemSucesso();
+        if(!empty($mensagemDeSucesso))
+        {
+            echo $mensagemDeSucesso;
+        }
+
+        $mensagemDeErro = obterMensagemErro();
+        if(!empty($mensagemDeErro))
+        {
+            echo $mensagemDeErro;
+        }
+    ?>
+    <p>Seu nome: <input type="text" name="nome" /></p>
+    <p>Sua idade: <input type="text" name="idade" /></p>
+    <p><input type="submit" value="Enviar dados do competidor"/></p>
+</form>
+
 </body>
+
 </html>
--- script.php
+++ script.php
@@ -1,12 +1,17 @@
 <?php 
 
-$categorias = [];
+
+/*$categorias = [];
 $categorias[] = 'infantil';
 $categorias[] = 'adolescente';
 $categorias[] = 'adulto';
-$categorias[] = 'idoso';
+$categorias[] = 'idoso';*/
 
 //print_r($categorias);
+
+include "servicos/servicoMensagemSessao.php";
+include "servicos/servicoCategoriaCompetidor.php";
+include "servicos/servicoValidacao.php";
 
 $nome = $_POST['nome'];
 $idade = $_POST['idade'];
@@ -15,33 +20,41 @@ $idade = $_POST['idade'];
 //var_dump($idade);
 
 //Restrinções ******************************************************
-if(empty($nome))
+/*if(empty($nome))
 {
-    echo'O nome não pode ser vazio';
+    $_SESSION['mensagem-de-erro'] = 'O nome não pode ser vazio, por favor preencha - o novamente';
+    header('location: index.php');
     return;
 }
-if (strlen($nome) < 3) // strlen averigua a condição
+else if (strlen($nome) < 3) // strlen averigua a condição
 {
-    echo'O nome deve conter mais de 3 caracteres';
-    return;
+    $_SESSION['mensagem-de-erro'] = 'O nome deve conter mais de 3 caracteres';
+    header('location: index.php');
+    return;    
 }
-if(strlen($nome) > 40)
+else if(strlen($nome) > 40)
 {
-    echo'O nome deve conter até 40 caracteres';
+    $_SESSION['mensagem-de-erro'] = 'O nome deve conter até 40 caracteres';
+    header('location: index.php');
     return;
 }
 if( ! is_numeric($idade)) // se não for numerico não sera aceito
 {
-    echo'Informe um número para a idade';
+    $_SESSION['mensagem-de-erro'] = 'Informe um número para a idade';
+    header('location: index.php');
     return;
-}
+}*/
 // ***************************************************************
 
-if ($idade >= 6 && $idade <= 12) {
+/*if ($idade >= 6 && $idade <= 12) {
     echo "O nadador " .$nome. " compete na categoria " . $categorias[0];
 } elseif ($idade >= 13 && $idade <= 18) {
     echo "O nadador ".$nome. " compete na categoria " . $categorias[1];
 } else {
     echo "O nadador " .$nome. " compete na categoria " . $categorias[2];
-}
+}*/
+
+defineCategoriaCompetidor($nome, $idade);
+
+header('location: index.php');
 ?>
